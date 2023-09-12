5da273d5aeccf65c0b4e5386e7832e4f361f5fab
vaishalirajesh <vaishali.patel@onlinepsbloans.com>
2023-09-12 10:02:12 +0530

    Vaishali_12_09_2023 Resolve login screen error

--- lib/view/screen/auth/login/login_view.dart
+++ lib/view/screen/auth/login/login_view.dart
@@ -22,91 +22,93 @@ class LoginPage extends StatelessWidget {
       child: Scaffold(
         backgroundColor: ColorConfig.jsCreamColor,
         extendBody: true,
-        body: Padding(
-          padding: EdgeInsets.all(20.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 50.h,
-              ),
-              SvgPicture.asset(
-                AppImages.jsLogo,
-                height: 50.h,
-                width: 60.sw,
-              ),
-              // SizedBox(
-              //   height: 20.h,
-              // ),
-              // Text(
-              //   AppString.startJourneyText,
-              //   style: StyleConfig.boldLargeText.copyWith(color: ColorConfig.jsBlackColor),
-              // ),
-              RichText(
-                textAlign: TextAlign.start,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: AppString.janText,
-                      style: StyleConfig.boldExtraLargeText,
-                    ),
-                    TextSpan(
-                      text: AppString.surakshaText,
-                      style: StyleConfig.boldExtraLargeText.copyWith(color: ColorConfig.jsBlueColor),
-                    ),
-                  ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50.h,
                 ),
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Text(
-                AppString.insurenceScheme,
-                style: StyleConfig.smallText.copyWith(
-                    color: ColorConfig.jsTextBlueGreyColor,
-                    fontFamily: JSFonts.outfitRegular,
-                    fontWeight: FontWeight.w300),
-              ),
-              SizedBox(
-                height: 70.h,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  AppString.mobileNumber,
-                  style: StyleConfig.regularText16.copyWith(
-                    color: ColorConfig.jsPrimaryColor,
-                    fontFamily: JSFonts.outfitRegular,
+                SvgPicture.asset(
+                  AppImages.jsLogo,
+                  height: 50.h,
+                  width: 60.sw,
+                ),
+                // SizedBox(
+                //   height: 20.h,
+                // ),
+                // Text(
+                //   AppString.startJourneyText,
+                //   style: StyleConfig.boldLargeText.copyWith(color: ColorConfig.jsBlackColor),
+                // ),
+                RichText(
+                  textAlign: TextAlign.start,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: AppString.janText,
+                        style: StyleConfig.boldExtraLargeText,
+                      ),
+                      TextSpan(
+                        text: AppString.surakshaText,
+                        style: StyleConfig.boldExtraLargeText.copyWith(color: ColorConfig.jsBlueColor),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15.h,
-              ),
-              Obx(() {
-                return AppTextField(
-                  hintText: AppString.enterMobileNumber,
-                  controller: loginLogic.mobileController,
-                  isReadOnly: false,
-                  isMandatory: false,
-                  isAutoFocus: true,
-                  inputType: TextInputType.phone,
-                  maxLength: 10,
-                  onChanged: loginLogic.onChangeMobile,
-                  prefixText: '+91  ',
-                  errorText: loginLogic.errorMsg.value,
-                );
-              }),
-              SizedBox(
-                height: 15.h,
-              ),
-              AppButton(
-                onPress: loginLogic.onPressSentOTP,
-                title: AppString.sentOTP,
-                isButtonEnable: true.obs,
-                isDataLoading: false.obs,
-              )
-            ],
+                SizedBox(
+                  height: 5.h,
+                ),
+                Text(
+                  AppString.insurenceScheme,
+                  style: StyleConfig.smallText.copyWith(
+                      color: ColorConfig.jsTextBlueGreyColor,
+                      fontFamily: JSFonts.outfitRegular,
+                      fontWeight: FontWeight.w300),
+                ),
+                SizedBox(
+                  height: 70.h,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    AppString.mobileNumber,
+                    style: StyleConfig.regularText16.copyWith(
+                      color: ColorConfig.jsPrimaryColor,
+                      fontFamily: JSFonts.outfitRegular,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Obx(() {
+                  return AppTextField(
+                    hintText: AppString.enterMobileNumber,
+                    controller: loginLogic.mobileController,
+                    isReadOnly: false,
+                    isMandatory: false,
+                    isAutoFocus: true,
+                    inputType: TextInputType.phone,
+                    maxLength: 10,
+                    onChanged: loginLogic.onChangeMobile,
+                    prefixText: '+91  ',
+                    errorText: loginLogic.errorMsg.value,
+                  );
+                }),
+                SizedBox(
+                  height: 15.h,
+                ),
+                AppButton(
+                  onPress: loginLogic.onPressSentOTP,
+                  title: AppString.sentOTP,
+                  isButtonEnable: true.obs,
+                  isDataLoading: false.obs,
+                )
+              ],
+            ),
           ),
         ),
       ),
